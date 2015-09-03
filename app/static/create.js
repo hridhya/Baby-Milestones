3888552a4908bdddbf88ce2c2940988acdeb1e21
hridhya <menonhridhya@gmail.com>
2015-09-03 11:08:56 -0700

    Added new files

---
+++ app/static/create.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$("#submitbutton").click(function () {
+     			var msg = $('#datepicker').val() + "\n" + $('#title').val() + "\n" + $('#details').val();
+					
+   	  			$.ajax({
+    				url: "/add_milestone",
+    				type: "POST",
+    				data: JSON.stringify(msg),
+    				contentType: "application/json; charset=utf-8",
+    				success: function(dat) { console.log(dat); }
+				});
+     		});
+
+
+
+
+});
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from app import app
 
 
@@ -6,3 +6,29 @@ from app import app
 @app.route('/index')
 def index() :
 	return render_template("index.html")
+	
+@app.route('/album_index')
+def album_index() :
+	return render_template("album_index.html")
+	
+@app.route('/album')
+def album() :
+	return render_template("album.html")	
+
+	
+@app.route('/start_album', methods=['POST'])
+def start_album() :
+	data = str(request.get_json())
+	words = data.split("\n")
+	print words
+	return data
+	
+@app.route('/add_milestone', methods=['POST'])
+def add_milestone() :
+	data = str(request.get_json())
+	words = data.split("\n")
+	print words
+	return data
+	
+	
+	
