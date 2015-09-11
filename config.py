f11e065f99dc1b1996a5a5d784b251735aee4322
hridhya <menonhridhya@gmail.com>
2015-09-11 11:38:24 -0700

    app_settings

--- config.py
+++ config.py
@@ -13,19 +13,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI =os.environ['DATABASE_URL']
 
-class ProductionConfig(Config):
-    DEBUG = False
 
-
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class TestingConfig(Config):
-    TESTING = True
