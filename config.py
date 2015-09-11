f4135d6a2d68618771b9cd8ec584c0c8ede3b0ba
hridhya <menonhridhya@gmail.com>
2015-09-11 10:40:46 -0700

    db

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ OPENID_PROVIDERS = [
     
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
