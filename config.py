68868fce2c21fe32e8c9a14bf5e257bf90265df0
hridhya <menonhridhya@gmail.com>
2015-09-11 11:21:03 -0700

    heroku

--- config.py
+++ config.py
@@ -9,7 +9,5 @@ OPENID_PROVIDERS = [
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
     
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI =os.environ['DATABASE_URL']
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
