09f955db7359f01f042eadbd4a7625161077fb37
hridhya <menonhridhya@gmail.com>
2015-09-11 14:00:58 -0700

    new files for correcting db migration

--- config.py
+++ config.py
@@ -12,5 +12,4 @@ OPENID_PROVIDERS = [
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI =os.environ['DATABASE_URL']
-
-
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
--- manage.py
+++ manage.py
@@ -1,15 +1,21 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 import os
 
-from app import app, db
-app.config.from_object('config')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-manager = Manager(app)
 
+manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(128))
 
 if __name__ == '__main__':
     manager.run()
-
