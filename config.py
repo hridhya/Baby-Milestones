bc638e0d05eab4e28496d497b213dacd7ef9fc78
hridhya <menonhridhya@gmail.com>
2015-09-12 20:41:26 -0700

    added local db

--- config.py
+++ config.py
@@ -11,5 +11,6 @@ OPENID_PROVIDERS = [
     
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+###SQLALCHEMY_DATABASE_URI = "postgresql://localhost/baby_app"
 SQLALCHEMY_DATABASE_URI =os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
