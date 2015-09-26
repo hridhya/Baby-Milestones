fdd469995c77875e313d0fd0a35e558063661d44
hridhya <hridhya@users.noreply.github.com>
2015-09-26 14:43:49 -0700

    Delete forms.py.py

--- app/forms.py.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from flask.ext.wtf import Form
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class LoginForm(Form) :
-	openid = StringField('openid', validators=[DataRequired()])
-	
